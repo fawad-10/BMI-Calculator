3b2c5f057726e4ae5eae241b9b360b2fb6d35fe5
Ahmed Fawad Awan <106027664+fawad-10@users.noreply.github.com>
2024-02-04 00:27:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -27,11 +27,12 @@ button.addEventListener("click", () => {
   if (height_status && weight_status) {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
-    if (bmi > 18.6) {
+    if (bmi < 18.6) {
       result.innerHTML = "Under-weight : " + bmi;
     } else if (bmi >= 18.6 && bmi < 24.9) {
       result.innerHTML = "Normal : " + bmi;
-    } else if (bmi <= 24.9) result.innerHTML = "Over-weight : " + bmi;
+    } else
+    {result.innerHTML = "Over-weight : " + bmi;}
   } else {
     alert("The form has error!");
     result.innerHTML = "";
